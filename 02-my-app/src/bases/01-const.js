81ff424ceb28b9f0b296c81e6ecedeb31d7029f3
aurora <auroraospina36@gmail.com>
2022-08-01 11:32:59 -0500

    Se agrega el proyecto

---
+++ 02-my-app/src/bases/01-const.js
@@ -0,0 +1,15 @@
+//variables y constantes
+
+const nombre = "Fernando";
+let apellido = "Herrera"
+let valorDado = 5;
+ valorDado = 7;
+
+console.log(nombre,apellido,valorDado)
+
+if (true){
+    const nombre = "Piter";
+    let valorDado = 6;
+    console.log(nombre)
+}
+console.log(valorDado)
